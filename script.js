fca09d17e226152ddaabb8ed2e5ea7368e8c80f9
karmas1207 <karomassudi@live.se>
2021-04-15 16:51:22 +0200

    Andra commiten
    Lagt till tid, kalender samt facebook. En nyhetsflik samt fixat ikonerna.

--- script.js
+++ script.js
@@ -12,3 +12,19 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
